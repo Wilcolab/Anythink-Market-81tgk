d24f761df9f7575aafd2ec8d738469ce8f3b8b8a
Michael Kirl <michaelkirl@Michaels-Air-2.lan>
2022-11-01 21:17:00 -0500

    linted file

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -50,7 +50,6 @@ const LoggedInView = (props) => {
 };
 
 const Header = (props) => {
-
     return (
         <nav
             className="navbar navbar-expand-md navbar-dark"
@@ -69,24 +68,4 @@ const Header = (props) => {
     )
 }
 export default Header;
-// class Header extends React.Component {
-//   render() {
-//     return (
-//       <nav
-//         className="navbar navbar-expand-md navbar-dark"
-//         style={{ padding: "0.5rem 2rem" }}
-//       >
-//         <Link to="/" className="navbar-brand">
-//           <img alt="logo" src={logo} />
-//         </Link>
-//
-//         {this.props.currentUser ? (
-//           <LoggedInView currentUser={this.props.currentUser} />
-//         ) : (
-//           <LoggedOutView currentUser={this.props.currentUser} />
-//         )}
-//       </nav>
-//     );
-//   }
-// }
 
