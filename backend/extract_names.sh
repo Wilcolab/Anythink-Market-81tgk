6884351102228125f1bfca996c88ad7da7af80ee
Michael <91095398+mkirl@users.noreply.github.com>
2023-02-06 01:39:35 +0000

    extracted file variable

--- backend/extract_names.sh
+++ backend/extract_names.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+touch names.txt
+
 file="$1"
 
-awk -F, '{if (tolower($4) ~ /amazon.com/) {print $2, $3}}' file >> names.txt
+awk -F, '{if (tolower($4) ~ /amazon.com/) {print $2, $3}}' file >> ./names.txt
