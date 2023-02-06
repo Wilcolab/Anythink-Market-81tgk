04a828451d251f2a1803ebd7b70cb147b489c5a2
Michael <91095398+mkirl@users.noreply.github.com>
2023-02-06 01:52:38 +0000

    test to see if its the platform

--- backend/scripts/extract_names.sh
+++ backend/scripts/extract_names.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-touch names.txt
+# touch names.txt
 
-file="$1"
+# file="$1"
 
-awk -F, '{if (tolower($4) ~ /amazon.com/) {print $2, $3}}' file >> ./names.txt
+# awk -F, '{if (tolower($4) ~ /amazon.com/) {print $2, $3}}' file >> ./names.txt
